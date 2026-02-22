c83402d1b0acb15a3af24e582e9aa622c0120be4
= <pnymeet@gmail.com>
2026-02-22 15:00:00 +0500

    js intro

---
+++ web/javascript/main.js
@@ -0,0 +1,28 @@
+// All special characters
+// 1. [!-\/:-@[-`{-~]
+// 2. [\p{P}\p{S}]
+// 23455424
+// 0380-0000000
+
+// 6    2    3    0
+// 10 x 10 x 10 x 10
+
+
+
+// 0001
+// 0002
+// 0009
+// 0010
+console.log("OK")
+/// data types
+// number = 10 , 3.455, 444444444444444444
+// string = "My name", 'M'
+// object = { name: "Ali", age: 34 }
+// object.name, object.age
+// console.log(object.name, object.age)
+
+// index no    0       1        2
+// array  = ["Red", "Green", "Blue"]
+// console.log(array[0])
+// loop(array)
+// console.log(array.length)
