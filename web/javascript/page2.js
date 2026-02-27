817809e77af14740ce9dc535bcb718a0745ab803
pny <pnymeet@gmail.com>
2026-02-27 14:30:25 +0500

    function, variable, dom

---
+++ web/javascript/page2.js
@@ -0,0 +1,33 @@
+var a = "Value is ";
+var b = 20;
+
+console.log(a+b);
+
+// x + y + z + xyz
+x = 5
+y = 10
+z = 15
+
+var result = x + y + z + (x*y*z);
+console.log("Expression result is ", result);
+
+// temp conversion C ===> F
+// °C * 9/5 + 32
+// cToF();
+// function cToF(){
+//     var c = 30;
+//     var result = c * 9/5 + 32;
+//     console.log("Temp in °F ", result);
+// }
+
+
+function cToF(){
+    var c = document.getElementById("cel").value;
+
+    var result = parseInt(c) * 9/5 + 32;
+    // console.log("Temp in °F ", result);
+    
+    var f = document.getElementById("result");
+    
+    f.innerText = `Temp in °F ${result}`;
+}
