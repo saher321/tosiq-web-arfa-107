bd18060022a048fcf7cf63b19a425bf6f50d2643
= <pnymeet@gmail.com>
2026-02-28 14:54:38 +0500

    let var const difference

---
+++ web/javascript/let-var-const.js
@@ -0,0 +1,19 @@
+var data = 10;
+var data = 20;
+// console.log(data);
+
+let data2 = 30;
+    data2 = 20;
+// console.log(data2);
+
+const data3 = 100;
+    //   data3 = 200;
+console.log(data3);
+
+// var   :: var can re-declared & re-initialized a variable
+// let   :: let can't be re-declared but re-initialized a variable
+// const :: const can't be re-declared and can't re-initialized a variable
+
+// scope
+// var has global scope
+// where let and const has blocked scope
