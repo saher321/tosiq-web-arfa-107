4bd74415ab4d730e0ec2f9a1b979b3443f22b1e2
= <pnymeet@gmail.com>
2026-03-07 14:19:10 +0500

    first react project

---
+++ web/reactjs/firstproject/src/App.jsx
@@ -0,0 +1,18 @@
+// component
+// props
+// children
+// state
+// library / pkg => icons, axios, react-hot-toast
+// cmd => npm create vite@latest, npm run dev, npm install
+
+const App = () => {
+  return (
+    <div>
+      <h1>My First React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  )
+}
+
+export default App
+
