b4310195b5ba6b01f4c7a5aeca91ed3a6d1da74e
= <pnymeet@gmail.com>
2026-03-07 14:35:46 +0500

    button component

--- web/reactjs/firstproject/src/App.jsx
+++ web/reactjs/firstproject/src/App.jsx
@@ -5,11 +5,16 @@
 // library / pkg => icons, axios, react-hot-toast
 // cmd => npm create vite@latest, npm run dev, npm install
 
+import Button from "./components/Button"
+
 const App = () => {
   return (
     <div>
+      <Button />
       <h1>My First React App</h1>
+      <Button />
       <p>This is a simple React application.</p>
+      <Button />
     </div>
   )
 }
---
+++ web/reactjs/firstproject/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button>
+      Information
+    </button>
+  )
+}
+
+export default Button
