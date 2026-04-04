60ef60e53d1ffb5b486fa7ce35a61907f06da2e0
pny <pnymeet@gmail.com>
2026-04-04 17:28:17 +0500

    basic

--- web/php-laravel/php/page1.php
+++ web/php-laravel/php/page1.php
@@ -1,3 +1,16 @@
 <?php
-    echo "Hello from php"
+    echo "Hello from php" . "<br>";
+
+    $a = 10;
+    $b = 20;
+    echo $a+$b;
+
+    # sessions
+    # basic programming (ifelse, loops, arrays)
+    # Restful apis (mysql => xampp)
+    # oop
 ?>
+
+<!-- # Laravel
+# Restful apis + Relations
+# department + student -->
