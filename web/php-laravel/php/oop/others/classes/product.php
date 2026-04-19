fb63c91046875c05e87254815272e931deec663c
pny <pnymeet@gmail.com>
2026-04-19 17:43:40 +0500

    assignment slug in lower case

--- web/php-laravel/php/oop/others/classes/product.php
+++ web/php-laravel/php/oop/others/classes/product.php
@@ -4,7 +4,7 @@
         function get_products() {
         
             $data = [
-                ["id" => 101, "name" => "usb 32 hp", "price" => 340],
+                ["id" => 101, "name" => "Usb 32 hp", "price" => 340],
                 ["id" => 103, "name" => "t shirt", "price" => 40],
                 ["id" => 104, "name" => "nike air shoes", "price" => 34000]
             ];
@@ -12,7 +12,7 @@
             return $data;
         }
 
-        // input => nike air shoes
+        // input => Nike air shoes
         // output=> nike-air-shoes
         function slug($str) {
             $slug = str_replace(" ", "_" , $str);
